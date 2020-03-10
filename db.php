56aecd7f78f8ca1db93b368e6fc5ed5779868843
povstenko <vitaly.povstenko@gmail.com>
2020-03-10 16:40:44 +0200

    Upload logo

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 require "vendor/redbeanphp/rb-mysql.php";
-R::setup('mysql:host=localhost;dbname=integral_db', 'root', '');
+R::setup('mysql:host=localhost;dbname=stephub_db', 'root', '');
 
 session_start();
