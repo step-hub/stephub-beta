fbd9362716ee872c861212df8d59b94472c92dcc
Bohdan <nbohdan999@gmail.com>
2020-03-11 09:38:06 +0200

    create registration

--- registration.php
+++ registration.php
@@ -1,28 +1,76 @@
 <?php
+
 require "db.php";
 
 $data = $_POST;
-$error = "";
 
 if (isset($data['do_signup'])) {
-    if (false/*count emails in db > 0*/) {
-        $error = "This email or login is used";
-    } else {
-        // registration
+    //register
+    $errors = array();
+    if (trim($data['login']) == ''){
+        $errors[] = 'login field is empty!!!';
+    }
+
+    if (trim($data['email']) == ''){
+        $errors[] = 'email field is empty!!!';
+    }
+
+    if (trim($data['stud_num']) == ''){
+        $errors[] = 'stud_num field is empty!!!';
+    }
+
+    if (trim($data['telegram']) == ''){
+        $errors[] = 'telegram field is empty!!!';
+    }
+
+    if ($data['password'] == ''){
+        $errors[] = 'password field is empty!!!';
+    }
+
+    if ($data['password_confirmation'] != $data['password']){
+        $errors[] = "password does doesn't confirm!!!";
+    }
+
+    if (R::count("users", "login = ?", array($data['login'])) > 0){
+        $errors[] = "user with such login already exist!!!";
+    }
+
+    if (R::count("users", "email = ?", array($data['email'])) > 0){
+        $errors[] = "user with such email already exist!!!";
+    }
+
+    if (R::count("users", "telegram_username = ?", array($data['telegram'])) > 0){
+        $errors[] = "user with such telegram already exist!!!";
+    }
+
+    if (R::count("student_ids", "student_id_num = ?", array($data['stud_num'])) == 0){
+        $errors[] = "user with such student num can't register!!!";
+    }
+    else {
+        $student = R::findOne('student_ids', 'student_id_num LIKE ?', array($data['stud_num']));
+        if (R::count("users", "studentid_id = ?", array($student->id)) > 0){
+            $errors[] = "user with such student num has already registered!!!";
+        }
+    }
+
+    if (empty($errors)){
+        //registration
         $user = R::dispense('users');
         $user->login = $data['login'];
-        $user->password = password_hash($data['password'], PASSWORD_DEFAULT);
         $user->email = $data['email'];
+        $user->studentid_id = $student->id;
         $user->telegram_username = $data['telegram'];
-        $user->studentid_id = 1;
-        $user->user_status = 1;
-        $user->is_online = true;
-        $user->reg_date = 1;
+        $user->password = password_hash($data['password'], PASSWORD_DEFAULT);
+        $user->is_online = 1;
+        $user->user_status = 3;
+        $user->reg_date = time();
+
         R::store($user);
 
-        $_SESSION['logged_user'] = $user;
-        header('location: index.php');
+    } else {
+        $error = $errors[0];
     }
+
 }
 ?>
 
@@ -50,26 +98,29 @@ if (isset($data['do_signup'])) {
 <body class="text-center">
 	<form class="form-login" action="registration.php" method="POST">
 
-        <p class="mt-5 mb-3 font-weight-bold text-danger"><?= $error ?></p>
+        <p class="mt-5 mb-3 font-weight-bold text-danger"><?php echo @$errors[0]?></p>
 		<a href="index.php">
 			<img class="mb-4" src="img/logo.jpg" alt="square logo" width="72" height="72">
 		</a>
 		<h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
 
 		<label for="inputLogin" class="sr-only">Login</label>
-		<input type="login" id="inputLogin" name="login" class="form-control" placeholder="Login" required autofocus>
+		<input type="login" id="inputLogin" name="login" value="<?php echo @$data['login']; ?>" class="form-control" placeholder="Login" required autofocus>
 
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required>
+        <input type="email" id="inputEmail" name="email" value="<?php echo @$data['email']; ?>" class="form-control" placeholder="Email address" required>
 
         <label for="inputStudNum" class="sr-only">Student ID</label>
-        <input type="text" id="inputStudNum" name="stud_num" class="form-control" placeholder="Student ID" required>
+        <input type="text" id="inputStudNum" name="stud_num" value="<?php echo @$data['stud_num']; ?>" class="form-control" placeholder="Student ID" required>
 
         <label for="inputTelegram" class="sr-only">Telegram ID</label>
-        <input type="text" id="inputTelegram" name="telegram" class="form-control" placeholder="Telegram ID" required>
+        <input type="text" id="inputTelegram" name="telegram" value="<?php echo @$data['telegram']; ?>" class="form-control" placeholder="Telegram ID" required>
 
 		<label for="inputPassword" class="sr-only">Password</label>
 		<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+
+		<label for="inputPassword" class="sr-only">Confirm password</label>
+		<input type="password" id="inputPassword" name="password_confirmation" class="form-control" placeholder="Confirm password" required>
 
 		<button class="btn btn-lg btn-primary btn-block" type="submit" name="do_signup">Sign up</button>
 		<p class="mt-5 mb-3">Already have an account? <a href="login.php">Log In</a></p>
