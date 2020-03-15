1f2465052f9c1a051ed5ac0c85cf7acd7729a2ff
povstenko <vitaly.povstenko@gmail.com>
2020-03-15 15:34:33 +0200

    Fix navbar color bug, fix header image size bug, add button

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,8 @@
 <div class="intro">
     <div class="container">
-        <div class="row pb-4">
+        <div class="row pb-2">
             <div class="col-md-3">
-                <img class="mx-auto d-block" src="img/logo.jpg" alt="big logo" width="200" height="200">
+                <img class="mx-auto d-block img-large-logo" src="img/logo.jpg" alt="big logo">
             </div>
             <div class="col-md-9 text-left">
                 <h2>Welcome to StepHub!</h2>
@@ -11,6 +11,10 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa fugiat laudantium, nihil perferendis recusandae. </p>
             </div>
         </div>
+        <div class="row pb-4">
+            <div class="col">
+                <a class="btn my-btn-dark shadow-sm" href="create-announcement.php">Add new announcement</a>
+            </div>
+        </div>
     </div>
 </div>
-
--- templates/navigation.php
+++ templates/navigation.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container">
         <a class="navbar-brand" href="index.php">StepHub</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
