d23fffeebbaea2ac3d79ca78eada04670da2c096
Bohdan <nbohdan999@gmail.com>
2020-03-17 04:14:37 +0200

    99 commit!!!

--- templates/header.php
+++ templates/header.php
@@ -5,10 +5,12 @@
                 <img class="mx-auto d-block img-large-logo" src="img/logo.jpg" alt="big logo">
             </div>
             <div class="col-md-9 text-left">
-                <h2>Hello, <?= $_SESSION['logged_user']['login']?>!</h2>
-                <h5>Lorem ipsum dolor sit amet, consectetur</h5>
-                <br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa fugiat laudantium, nihil perferendis recusandae. </p>
+                <h1>Зараз є 99 коммітів у дев)) Поміняй лого сайту на головній сторінці на якусь святкову картинку) і зроби 100-ий комміт. Потім змінимо)</h1>
+                <h2>P.S. Чекни 1 оголошення, коментарі вже працюють але треба робити багато перевірок ще</h2>
+<!--                <h2>Hello, --><?//= $_SESSION['logged_user']['login']?><!--!</h2>-->
+<!--                <h5>Lorem ipsum dolor sit amet, consectetur</h5>-->
+<!--                <br>-->
+<!--                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa fugiat laudantium, nihil perferendis recusandae. </p>-->
             </div>
         </div>
         <div class="row pb-4">
