379f71aa2dd89a7c50c415afaaabd4581039034f
povstenko <vitaly.povstenko@gmail.com>
2020-03-19 00:20:08 +0200

    Update logo

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row pb-2">
             <div class="col-md-3">
-                <img class="mx-auto d-block img-large-logo" src="img/logo.jpg" alt="big logo">
+                <img class="mx-auto d-block img-large-logo" src="img/logo.png" alt="big logo">
             </div>
             <div class="col-md-9 text-left">
                 <h2>Вітаємо, <?= $_SESSION['logged_user']['login']?>!</h2>
@@ -13,7 +13,7 @@
         </div>
         <div class="row pb-4">
             <div class="col">
-                <a class="btn btn-lg my-btn-dark shadow-sm" href="create-announcement.php"><i class="fas fa-plus mr-2"></i>Створити нове оголошення</a>
+                <a class="btn btn-lg my-btn-dark shadow-sm" href="create-announcement.php"><i class="fas fa-plus mr-2"></i>Розмістити оголошення</a>
             </div>
         </div>
     </div>
