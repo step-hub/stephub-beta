c7cd669775ccdf0044cc0348dbc2c62347d499df
Bohdan <nbohdan999@gmail.com>
2020-03-23 20:08:18 +0200

    Checking errors on sending mail page

--- mail.php
+++ mail.php
@@ -5,11 +5,19 @@ include_once "php/functions.php";
 if (array_key_exists('logged_user', $_SESSION) and $_SESSION['logged_user']->user_status < 3){
     $data_get = $_GET;
     $data_post = $_POST;
+    $errors = array();
 
     if (isset($data_post['do_send'])){
-        $user = R::findOne('users', 'id = ?', array($data_post['to']));
-        mail($user['email'], $data_post['subject'], $data_post['message'], 'From: stephub.com@gmail.com');
-        echo "<script>window.close()</script>>";
+        if ($data_post['subject'] == '')
+            $errors[] = 'Має бути написана тема повідомлення';
+        if ($data_post['message'] == '')
+            $errors[] = 'Повідомлення не може бути пустим';
+
+        if (!$errors) {
+            $user = R::findOne('users', 'id = ?', array($data_post['to']));
+            mail($user['email'], $data_post['subject'], $data_post['message'], 'From: stephub.com@gmail.com');
+//            echo "<script>window.close()</script>>";
+        }
     }
     elseif (!array_key_exists('id', $data_get)){
         header("location: index.php");
@@ -43,12 +51,15 @@ if (array_key_exists('logged_user', $_SESSION) and $_SESSION['logged_user']->use
 <?php include_once 'templates/navbar.php'; ?>
 <?php if (array_key_exists('logged_user', $_SESSION) and $_SESSION['logged_user']->user_status < 3): ?>
     <div class="container">
+        <?php if($errors): ?>
+            <p class="mt-0 mb-0 font-weight-bold text-danger"><?= @$errors[0]; ?></>
+        <?php endif; ?>
         <form action="mail.php" method="POST" class="form-group">
-            <input type="text" name="to" class="form-control" value="<?= $data_get['id']?>" hidden>
+            <input type="text" name="to" class="form-control" value="<?= $data_get ? $data_get['id'] : $data_post['to'] ?>" hidden>
             <label for="mail_subject">Тема</label>
-            <input type="text" name="subject" id="mail_subject"  class="form-control">
+            <input type="text" name="subject" id="mail_subject" value="<?= $data_post ? $data_post['subject'] : ''?>" class="form-control">
             <label for="mail_message">Повідомлення</label>
-            <textarea name="message" id="mail_message" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="message" id="mail_message" cols="30" rows="10" class="form-control"><?= $data_post ? $data_post['message'] : ''?></textarea>
             <button name="do_send" type="submit" class="btn btn-info">Відправити</button>
         </form>
     </div>
