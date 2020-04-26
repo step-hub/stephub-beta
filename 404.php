66f5a99ee62233dcb34fb0778d76a8f85931a048
Vitaly Povstenko <vitaly.povstenko@gmail.com>
2020-04-26 19:48:22 +0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ua">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Сторінку не знайдено | StepHub</title>
+
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google Material Design Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Font Awesome Icons -->
+    <link href="vendor/fontawesome-free-5.9.0-web/css/all.css" rel="stylesheet">
+
+    <!-- load all styles -->
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/announcement.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+    <!-- Preloader -->
+    <?php include_once 'templates/preloader.html'; ?>
+
+    <!-- Navigation -->
+    <?php include_once 'templates/navbar.php'; ?>
+
+    <!-- Page Content -->
+    <div class="container p-0 p-md-3 pt-md-5 ">
+        <div class="card not-found-parent diagonal-gradient-gray-light shadow border-xs-0">
+            <div class="card-body text-center p-0 p-md-3">
+                <div class="card not-found-child diagonal-gradient-gray-light border-xs-0">
+                    <div class="card-body text-center my-color-dark py-5">
+                        <h1>404</h1>
+                        <h3 class="mb-5">Вибачте, сторінку не знайдено</h3>
+                        <a href="index.php" class="btn my-btn-outline-dark">Повернутись на головну</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <?php include_once 'templates/footer.php'; ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Main sctipt -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
